c93025917657a6201d2af219cd8dc90dcc66b522
Nate Allen <nate@linchpin.com>
2020-01-07 14:10:56 -0500

    Change up the order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Go to the working directory (current directory by default)
-cd ${working_directory:-./}
-
 # If a URL is passed to an exclude list, remove ours and use theirs.
 if [[ ! -z "${exclude_list_url}" ]]; then
 	rm exclude-list.txt
     wget -O exclude-list.txt "${exclude_list_url}"
 fi
 
-# Loop over list of files/folders and remove them from deployment
 ITEMS=`cat exclude-list.txt`
+
+# Go to the working directory (current directory by default)
+cd ${working_directory:-./}
+
+# Loop over list of files/folders and remove them from deployment
 for ITEM in $ITEMS; do
     find . -depth -name "$ITEM" -exec rm -rf "{}" \;
 done
